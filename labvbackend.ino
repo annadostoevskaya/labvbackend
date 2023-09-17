6f2b457708b631b1c17d67c8d6626965af825557
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2023-09-17 19:27:30 +0600

    ina219

--- labvbackend.ino
+++ labvbackend.ino
@@ -1,4 +1,11 @@
 #include <stdint.h>
+#include <Adafruit_INA219.h>
+
+#define D_PRINT(var) do {     \
+    Serial.print(#var ": ");  \
+    Serial.print(var);        \
+    Serial.print('\n');       \
+  } while(0);
 
 // управление мнохроматором
 const int monochr_pin_1 = 10;
@@ -7,7 +14,47 @@ const int monochr_pin_3 = 12;
 const int monochr_pin_4 = 13;
 // int led_pin = 13; // пин, к которому подключен светодиод
 
-void setup() {
+void panic(const char *msg)
+{
+  Serial.println(msg);
+  while (1) { delay(10); }
+}
+
+uint8_t serial_wait_byte()
+{
+  while (Serial.available() == 0);
+  return (uint8_t)Serial.read();
+}
+
+template <typename T>
+T serial_get_data()
+{
+  T data = 0;
+  size_t byte_count = 0;
+  while (byte_count == 0) 
+  {
+    byte_count = Serial.readBytes(reinterpret_cast<uint8_t*>(&data), sizeof(T));
+  }
+
+  // D_PRINT(byte_count);
+  // D_PRINT(data);
+  
+  return data;
+}
+
+Adafruit_INA219 *g_ina219;
+
+void setup() 
+{
+  Serial.begin(115200);
+  
+  g_ina219 = new Adafruit_INA219();
+  
+  if (!g_ina219->begin()) 
+    panic("Failed to find INA219 chip!");
+
+  g_ina219->setCalibration_16V_400mA();
+  
   // Устанавливаем пин как выход для управления монохроматором
   pinMode(monochr_pin_1, OUTPUT);
   pinMode(monochr_pin_2, OUTPUT);
@@ -21,19 +68,12 @@ void setup() {
   digitalWrite(monochr_pin_4, HIGH);
   
   // pinMode(led_pin, OUTPUT); //светодиод
- 
-  Serial.begin(9600);
-}
-
-uint8_t wait_byte()
-{
-  while (Serial.available() == 0);
-  
-  return (uint8_t)Serial.read();
 }
 
 ////////////////////////////////////////////////////////
 // monochr.c
+#define MONOCHR_TO_NM 75 // NOTE(annad): monochr.h, rotate = 75 ~ 1nm
+
 void rotate_monochr(int first, int second, int third, int fours)
 {
 
@@ -44,16 +84,21 @@ void rotate_monochr(int first, int second, int third, int fours)
 
 void loop() 
 {
-  char command_pc = wait_byte();
+  float busV = g_ina219->getBusVoltage_V();
+  float mV = g_ina219->getShuntVoltage_mV();
+  float mA = g_ina219->getCurrent_mA();
+  D_PRINT(busV);
+  D_PRINT(mA);
+  D_PRINT(mV);
+  return;
   
+  char command_pc = serial_get_data<char>();
+ 
   // digitalWrite(led_pin, HIGH); // включаем светодиод 
   // delay(250); // ждем
-
-  uint16_t rotate = 0;
-  ((uint8_t*)&rotate)[1] = wait_byte();
-  ((uint8_t*)&rotate)[0] = wait_byte();
-  rotate *= 75; // NOTE(annad): monochr.h, rotate = 75 ~ 1nm
-
+  uint16_t rotate = MONOCHR_TO_NM * serial_get_data<uint16_t>();
+  return;
+    
   bool processing = true;
   while (processing)
   {
@@ -61,6 +106,7 @@ void loop()
     {
       case 'L':
       {
+        uint16_t rotate = MONOCHR_TO_NM * serial_get_data<uint16_t>();
         for (int i = 0; i <= rotate; i++) 
         {
           // Включаем 4 обмотку, отключаем 2
@@ -89,6 +135,7 @@ void loop()
           
       case 'R':
       {
+        uint16_t rotate = MONOCHR_TO_NM * serial_get_data<uint16_t>();
         for (int i = 0; i <= rotate; i++) {    
           // Включаем 1 обмотку, отключаем 3
           digitalWrite(monochr_pin_1, LOW);
